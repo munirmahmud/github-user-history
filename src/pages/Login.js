51fd743703d6832084dcea90fcaeb52833dec9c0
Munir Mahmud <munirmahmud6@gmail.com>
2020-12-14 08:39:24 +0600

    User login page markup & styling done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import loginImg from '../images/github-user.svg';
 
 const Login = () => {
     return (
-        <div>
-            Login Page
-        </div>
+        <Container>
+            <Wrapper>
+                <img src={loginImg} alt="Github User Login" />
+                <h1>Github User</h1>
+                <button className="btn">Login</button>
+            </Wrapper>
+        </Container>
     )
-}
+};
 
-export default Login
+const Container = styled.section`
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+`;
+
+const Wrapper = styled.div`
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+
+    img {
+        margin-bottom: 2rem;
+        display: block;
+        width: 100%;
+    }
+    h1 {
+        margin-bottom: 1.5rem;
+    }
+`;
+
+export default Login;
